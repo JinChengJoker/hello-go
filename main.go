17bcfec9456f5fb00c8a66cd55898f07b40b8223
Jin Cheng <jinchengjoker@foxmail.com>
2023-11-29 17:37:45 +0800

    gogogo

--- main.go
+++ main.go
@@ -2,6 +2,34 @@ package main
 
 import "fmt"
 
+func add(x int, y int) int {
+	return x + y
+}
+
+func swap(x string, y string) (string, string) {
+	return y, x
+}
+
+func split(sum int) (x int, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
+func zeroValue(value int) {
+	value = 0
+}
+
+func zeroPtr(prt *int) {
+	*prt = 100
+}
+
 func main() {
-	fmt.Println("hello")
+	m := map[string]int{
+		"apple":  100,
+		"banana": 200,
+	}
+
+	fmt.Println(m)
+	fmt.Println(len(m))
 }
